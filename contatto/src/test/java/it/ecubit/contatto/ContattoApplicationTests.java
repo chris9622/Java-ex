0fdf581fbac5259bc80d0b38a3c001859386dfb3
Christian Mastropietro <christianmastropietro@AirdiChristian.station>
2024-02-01 11:28:16 +0100

    commit project 6

---
+++ contatto/src/test/java/it/ecubit/contatto/ContattoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ecubit.contatto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContattoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
